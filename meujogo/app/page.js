9e08de0392012ccdda83556e329f315193ad909c
ArturLima33 <169162600+ArturLima33@users.noreply.github.com>
2026-04-02 22:31:52 +0000

    deado

--- meujogo/app/page.js
+++ meujogo/app/page.js
@@ -1,6 +1,6 @@
 'use client';
 import { useState, useEffect } from 'react';
-import Dado from '../components/dado';
+import Dado from './components/dado.js';
 
 export default function JogoDados() {
   const [rodada, setRodada] = useState(1);
